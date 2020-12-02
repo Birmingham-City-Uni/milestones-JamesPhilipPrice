d38dae5b651ea69aa79493e1a23d70632711dfae
JamesPhilipPrice <55580682+JamesPhilipPrice@users.noreply.github.com>
2020-12-02 18:17:24 +0000

    Added the basic setup for an inventory

---
+++ CPPFGD_James_Price/CPPFGD_James_Price/BulletManager.h
@@ -0,0 +1,96 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+
+#include "Player.h"
+#include "Target.h"
+#include "EnemyAIManager.h"
+#include "Keys.h"
+
+#define PI 3.14159265
+
+class BulletManager {
+public:
+	BulletManager(SDL_Renderer* _renderer, Player* _player, EnemyAIManager* _enemies) : renderer(_renderer), player(_player), enemyManager(_enemies) {}
+
+	struct Bullet {
+		float x, y, rotation, distance;
+	};
+
+	void Init() {
+		SDL_Surface* surface = IMG_Load("assets/bullet.png");
+		this->bulletTexture = SDL_CreateTextureFromSurface(this->renderer, surface);
+		SDL_FreeSurface(surface);
+	}
+
+	void ProcessInput(bool _keys[]) {
+		if (_keys[MOUSELEFT]) {
+			if (SDL_GetTicks() - lastShot > SHOOT_TIMER_MS) {
+				std::cout << "Shooting!" << std::endl;
+				float x = player->GetOriginX();
+				float y = player->GetOriginY();
+				bullets.push_back(Bullet{ x, y, player->GetAngle(), 0.0f });
+				lastShot = SDL_GetTicks();
+			}
+		}
+	}
+
+	int Update() {
+		int scoreVal = 0;
+		for (auto& b : bullets) {
+			b.x += sin(b.rotation * PI / 180.0f) * BULLET_VELOCITY;
+			b.y -= cos(b.rotation * PI / 180.0f) * BULLET_VELOCITY;
+			b.distance += BULLET_VELOCITY;
+
+			SDL_Rect bulletRect = {b.x, b.y, 10, 10};
+			SDL_Rect nullRect;
+			//Check all of the enemies
+			for (auto& i : enemyManager->GetEnemies()) {
+				if (SDL_IntersectRect(&bulletRect, i->GetEntityRect(), &nullRect)) {
+					//Deals damage to the enemy and checks if it is dead
+					if (!(i->TakeDamage(10))) {
+						i->UnlockChest();
+						std::cout << "The enemy has died and their inventory is now unlocked" << std::endl;
+					}
+					scoreVal++;
+					b.distance = 1000;
+					break;
+				}
+				if (b.distance >= 1000) {
+					continue;
+				}
+			}
+			
+		}
+
+		auto remove = std::remove_if(bullets.begin(), bullets.end(), [](const Bullet& b){return b.distance >= 1000; });
+		bullets.erase(remove, bullets.end());
+		return scoreVal;
+	}
+
+	void Draw() {
+		SDL_Point center = { 5, 5 };
+		for (auto& b : bullets) {
+			SDL_Rect dest = { b.x, b.y, 10, 10 };
+			SDL_RenderCopyEx(renderer, bulletTexture, 0, &dest, b.rotation, &center, SDL_FLIP_NONE);
+		}
+	}
+
+	void Clean() {
+		SDL_DestroyTexture(this->bulletTexture);
+	}
+
+
+private:
+	SDL_Renderer* renderer;
+	SDL_Texture* bulletTexture;
+	Player* player;
+	EnemyAIManager* enemyManager;
+	std::vector<Bullet> bullets;
+	const int SHOOT_TIMER_MS = 300;
+	const int BULLET_VELOCITY = 1;
+	unsigned int lastShot = 0;
+};
---
+++ CPPFGD_James_Price/CPPFGD_James_Price/Container.h
@@ -0,0 +1,33 @@
+#pragma once
+#include<iostream>
+#include "Entity.h"
+#include "Player.h"
+#include "Keys.h"
+
+enum LootType {
+	Empty = 0,
+	Food,
+	Water,
+	Bolts,
+	GunParts,
+	ENUM_C
+};
+
+class Container : public Entity{
+public:
+	Container(int _startX, int _startY, const char* _textureLocation, SDL_Renderer* _renderer, bool _movable, bool _damagable, float _health);
+	~Container();
+
+	LootType inventory[5][6];
+
+	void OpenChest();
+	void CloseChest();
+	void UnlockChest();
+	void CheckForOpenCondition(Player* _player, bool _keysInp[]);
+	void CheckForCloseCondition(Player* _player);
+
+private:
+	bool unlocked = true;
+	bool open;
+	Player* player;
+};
